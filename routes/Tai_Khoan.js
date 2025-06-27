8dbcfef16445ce736272d6dcff37e3429195cdea
TamPhamLor <congtam002005@gmail.con>
2025-06-27 08:47:08 +0700

    Luu

--- routes/Tai_Khoan.js
+++ routes/Tai_Khoan.js
@@ -10,5 +10,5 @@ router.get ('/LayTheoLoai/:ID' , Handle_Tai_Khoan.Get_ByLoai);
 router.post ('/Add' , Upload.Upload_Image__.single("Image"), Handle_Tai_Khoan.Add_Tai_Khoan);
 router.put ('/Edit/:ID' , Upload.Upload_Image__.single("Image") , Handle_Tai_Khoan.Edit_Tai_Khoan);
 router.delete ('/Delete/:ID' , Handle_Tai_Khoan.Delete_Tai_Khoan);
-router.get ('/Login/:Id_LoaiTaiKhoan' , Handle_Tai_Khoan.Check_Login);
+router.post ('/Login/:Id_LoaiTaiKhoan' , Handle_Tai_Khoan.Check_Login);
 module.exports = router;    
