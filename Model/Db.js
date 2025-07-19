b635e6cee9348a47afd7c041019a19b0991a1a27
TranPhanNhan <nhannhaongo1997@gmail.com>
2025-07-19 13:41:53 +0700

    Lưu

--- Model/Db.js
+++ Model/Db.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
-const uri = "mongodb://localhost:27017/WellNest";
+// const uri = "mongodb://localhost:27017/WellNest";
+const uri = "mongodb+srv://tranphannhan:oLVT47GDg2GMj9mb@cluster0.av1kh.mongodb.net/WellNest?retryWrites=true&w=majority"
+
 
 async function connectDB() {
   try {
